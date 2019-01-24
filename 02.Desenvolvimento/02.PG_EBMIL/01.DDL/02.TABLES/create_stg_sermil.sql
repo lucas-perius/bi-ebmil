d5e2f15eb5836e5de8a84adc1fffa4e28614b402
lucas-perius <46960201+lucas-perius@users.noreply.github.com>
2019-01-24 15:20:12 +0000

    0007 - Create file DDL table sermil.
    Criação do arquivo de DDL da tabela sermil.

---
+++ 02.Desenvolvimento/02.PG_EBMIL/01.DDL/02.TABLES/create_stg_sermil.sql
@@ -0,0 +1,26 @@
+CREATE TABLE stg_ebmil.stg_sermil
+(
+  cod_arquivo
+, ano_nascimento
+, mun_nascimento
+, uf_nascimento
+, pais_nascimento
+, zona_residencial
+, mun_residencial
+, uf_residencial
+, pais_residencial
+, num_altura
+, num_peso
+, num_calcado
+, tam_cabeca
+, tam_cintura
+, txt_sexo
+, txt_escolaridade
+, txt_religiao
+, txt_estado_civil
+, txt_jsm
+, mun_jsm
+, uf_jsm
+, ano_vinculacao
+, txt_dispensa
+)
